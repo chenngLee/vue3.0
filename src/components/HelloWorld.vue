e84384e9d496d332484c7134405b214bf5acd44e
chenngLee <licheng736560926@outlook.com>
2020-10-08 20:49:41 +0800

    vue 2.x 实现todoList

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,19 +1,49 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <div>
+    <form action>
+      <input type="text" v-model="stu.id" placeholder="id" />
+      <input type="text" v-model="stu.name" placeholder="name" />
+      <input type="text" v-model="stu.age" placeholder="age" />
+      <input type="submit" @click.prevent="addStu" />
+    </form>
+    <ul>
+      <li
+        v-for="(stu,index) in stus"
+        :key="index"
+        @click="remStu(index)"
+      >{{stu.name}} --- {{stu.age}}</li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
+  name: "HelloWorld",
   data() {
     return {
-      count: 0
+      stus: [
+        { id: 0, name: "zs", age: 10 },
+        { id: 1, name: "ls", age: 20 },
+        { id: 2, name: "ww", age: 60 }
+      ],
+      stu: {
+        id: "",
+        name: "",
+        age: ""
+      }
+    };
+  },
+  methods: {
+    remStu(index) {
+      this.stus = this.stus.filter((item, idx) => idx != index);
+    },
+    addStu() {
+      const stu = Object.assign({}, this.stu);
+      this.stus.push(stu);
+      this.stu.id = "";
+      this.stu.name = "";
+      this.stu.age = "";
     }
   }
-}
+};
 </script>
