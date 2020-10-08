5e6dac155e8f1c0d6e408b575397e197c067101c
chenngLee <licheng736560926@outlook.com>
2020-10-08 20:57:19 +0800

    vue 3.0 组合API

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld />
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld />
+    <CompositionApi />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import CompositionApi from "./components/CompositionApi.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    HelloWorld,
+    CompositionApi
   }
-}
+};
 </script>
